0ccbea36b6099688f4cfd93b13b1fb48c12813d2
Zainab Alsafwani <alzope43@gmail.com>
2024-07-30 21:36:18 +0300

    more utilis has been added

--- utils/general.py
+++ utils/general.py
@@ -3,3 +3,8 @@ import streamlit as st
 def new_line(n=1):
     for _ in range(n):
         st.write("\n")
+
+def get_hw_ratio(img):
+    h, w = img.shape[:2]
+    hw_ratio = h / w
+    return hw_ratio
--- utils/stamp.py
+++ utils/stamp.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
+from typing import Union
+from PIL import Image
 
 def get_roi(doc, stamp):
-    """
-    """
     h, w, _ = doc.shape
 
     starting_h = h - stamp.shape[0]
@@ -13,13 +14,64 @@ def get_roi(doc, stamp):
     ending_w = starting_w + stamp.shape[1]
 
     roi = doc[starting_h: ending_h, starting_w:ending_w, :]
-    return roi, starting_h, ending_h, starting_w, ending_w
+    return roi, starting_h, ending_h, starting_w, ending_w 
+
+
+def get_whitespace_ratio(window):
+    counter = 0
+    img = Image.fromarray(window.astype('uint8'), 'RGB')
+    img = img.convert("RGBA")
+    pixels = img.getdata()
+    for pixel in pixels:
+        # print(f"PIXEL: {pixel}")
+        if pixel == (255, 255, 255, 255):
+            counter += 1
+    return counter
+
+def get_max_idx(counter_dict):
+    lst = [window["counter"] for window in counter_dict.values()]
+    idx = lst.index(max(lst))
+    return idx
+    
+def whitespace_area(doc, stamp):
+    stamp_h, stamp_w = stamp.shape[:2]
+    doc_h, doc_w = doc.shape[:2]
+    
+    white_ratio_dict: dict[int, dict[str, Union[tuple, int]]] = {}
+    for starting_heigh in range(0, doc_h, stamp_h):
+        for starting_width in range(0, doc_w, stamp_w):
+            ending_width = starting_width + stamp_w
+            ending_heigh = starting_heigh + stamp_h
+            roi = doc[starting_heigh: ending_heigh, starting_width:ending_width, :]
+
+            dict_index = len(white_ratio_dict) #?
+            white_ratio_dict[dict_index] = {
+                "coord": (starting_heigh, ending_heigh, starting_width, ending_width),
+                "counter": get_whitespace_ratio(roi)
+            }
+    print(f"ALL DICT: {white_ratio_dict}")
+    max_idx = get_max_idx(white_ratio_dict)
+    print(f"MAX IDX: {max_idx}")
+    coord = white_ratio_dict[max_idx]["coord"]
+    starting_heigh, ending_heigh, starting_width, ending_width = coord
+    roi = doc[starting_heigh: ending_heigh, starting_width:ending_width, :]
+
+    return roi, starting_heigh, ending_heigh, starting_width, ending_width
 
 
 def add_stamp(doc, stamp, resize_show_ratio=0.75, imshow = False, imwrite = False):
-    dst = bgremove(stamp)
-    roi, starting_h, ending_h, starting_w, ending_w = get_roi(doc, stamp)
-    doc[starting_h: ending_h, starting_w:ending_w, :] = dst
+    #stamp = color_adjustment(stamp)
+    stamp = Image.fromarray(stamp.astype('uint8'), 'RGB')
+    stamp = background_removal(stamp)
+    stamp = stamp.convert('RGB') #i think this should be removed felt its unnecessry
+    stamp = np.array(stamp)
+    # stamp = cv2.imread(stamp)
+    # cv2.imshow(f"STAMP", stamp)
+    # if cv2.waitKey(0) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+
+    roi, starting_h, ending_h, starting_w, ending_w = whitespace_area(doc, stamp)
+    doc[starting_h: ending_h, starting_w:ending_w, :] = stamp
     if imshow:
         show_doc = doc.copy()
         show_doc = cv2.resize(show_doc, None, fx=resize_show_ratio, fy=resize_show_ratio)
@@ -32,17 +84,25 @@ def add_stamp(doc, stamp, resize_show_ratio=0.75, imshow = False, imwrite = Fals
 
     return doc
 
-def bgremove(image, min_thresh=127, max_thresh=255):
-    """
-    Background Removal
-    """
-    myimage_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, baseline = cv2.threshold(myimage_grey,min_thresh,max_thresh,cv2.THRESH_TRUNC)
-    _, background = cv2.threshold(baseline,min_thresh,max_thresh,cv2.THRESH_BINARY)
-    _, foreground = cv2.threshold(baseline,min_thresh,max_thresh,cv2.THRESH_BINARY_INV)
+def color_adjustment(image):
+    stamp_img=image
+    if stamp_img.shape[2] == 4:
+        stamp_img = cv2.cvtColor(stamp_img, cv2.COLOR_RGBA2BGR)
+    else:
+        stamp_img = cv2.cvtColor(stamp_img, cv2.COLOR_RGB2BGR)
+    return stamp_img
 
-    foreground = cv2.bitwise_and(image,image, mask=foreground)
-    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
+def background_removal(stamp):
+    stamp = stamp.convert("RGBA")
+    pixels = stamp.getdata()
+    newData = []
+ 
+    for item in pixels:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
 
-    finalimage = background+foreground
-    return finalimage
+    stamp.putdata(newData)
+    stamp.save("./New.png", "PNG")
+    return stamp
